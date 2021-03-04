0366f7a378b18ef9bb0b1f3aac0fd5a35fa87b02
HarbdhulQuadri <adegbijilanre@gmail.com>
2021-03-04 02:05:47 +0100

    sorry to include this but i am in love

--- controllers/manage.js
+++ controllers/manage.js
@@ -5,10 +5,10 @@ module.exports = function (router) {
     router.get('/', function (req, res) {        
         res.render('manage/index');
    });
-   router.get('books', function (req, res) {        
+   router.get('/books', function (req, res) {        
     res.render('manage/books/index');
 });
-router.get('categories', function (req, res) {        
+router.get('/categories', function (req, res) {        
     res.render('manage/categories/index');
 });
 
